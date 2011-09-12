c3850f2001d76f9da23f328ee4e42ba3f185bb75
tuent <tuent@applistar.com>
2011-09-12 11:15:33 +0700

    my first code

---
+++ first.py
@@ -0,0 +1,10 @@
+
+def main():
+	print "No one die as virgin"
+	print "Life fuck us all"
+	print "                 Kurt Cobain"
+	for i in range(0, 10):
+		print "Fuck this life"
+
+if __name == "__main__":
+	main()
